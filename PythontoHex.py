a668f595cbc5c872293d89cc2b5a518cee67a546
Wan Afifi Wan Zain <epy770@gmail.com>
2022-01-10 00:30:55 +0800

    Add files via upload

--- PythontoHex.py
+++ PythontoHex.py
@@ -1,8 +1,29 @@
-getinput = input("Give Data: ")
-getdata = ""
-for char in getinput:
-    data = hex(ord(char))
-    # print(data)
-    getdata = getdata + data
-    
-print(getdata.replace("0x", " "))
+
+import os
+import sys
+
+def exit(getinput):
+    if getinput.upper() == "EXIT":
+        os.system('clear')
+        data = input("Did you mean Exit is EXIT?? (Y/N): ")
+        if data.upper() == "Y":
+            sys.exit()
+        else:
+            return process(getinput)
+    return process(getinput)
+
+def process(getinput):
+    getdata = ""
+    for char in getinput:
+        data = hex(ord(char))
+        getdata = getdata + data
+
+    print(f"{getinput} is :",getdata.replace("0x", " "))
+    main()
+
+def main():
+    print("\nType 'EXIT' to close the program")
+    getinput = input("Give Data: ")
+    exit(getinput)
+
+main()
